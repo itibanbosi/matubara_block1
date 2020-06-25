d16592777c3d7307087ff1db19a9da5d4e83c716
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-25 17:51:09 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
-enum eureka_IO {
-  Aﾎﾟｰﾄ,
-  Bﾎﾟｰﾄ,
-  Cﾎﾟｰﾄ
-}
 
 
 
 
 //% color="#3943c6" block="ﾕｰﾚｶ車" icon="\uf1b9"
-namespace eureka_blocks_car {
+namespace matubara_blocks_car {
 
 
-  //% blockId=servos_backward
+  //% blockId=sport_habatobi
   //% block="幅跳び" group="スポーツテスト"
-  export function backward(): void {
-    return Math.map(input.acceleration(Dimension.Y), 0, 1100, 0, 90)
+  export function habatobi(): void {
+    return acceleration(Dimension.Y)/1100*90
   }
 
 

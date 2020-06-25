9d0165577a93ea7a3546a6f074d4eba0ccd85007
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-25 17:47:08 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ namespace eureka_blocks_car {
   //% blockId=servos_backward
   //% block="幅跳び" group="スポーツテスト"
   export function backward(): void {
-    Math.map(input.acceleration(Dimension.Y), 0, 1100, 0, 90)
+    return Math.map(input.acceleration(Dimension.Y), 0, 1100, 0, 90)
   }
 
 

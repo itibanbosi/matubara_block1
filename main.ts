ce5e5873c419108c4ef025bc4a49eee7b89c9a98
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-27 20:23:55 +0900

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,23 @@
-//% color="#3943c6" block="幅跳び" icon="\uf1b9"
+//% color="#3943c6" block="幅跳び"
 namespace matubara_blocks {
 
 
-  //% blockId=sport_habatobi
-  //% block="幅跳び" group="スポーツテスト"
-  export function habatobi():number{
-    return input.acceleration(Dimension.Y)/1100*90;
+  //% blockId=sport_habatobi_x
+  //% block="X角度" group="体育"
+  export function axis_x():number{
+    return input.acceleration(Dimension.X)/1100*90;
+  }
 
+ //% blockId=sport_habatobi_y
+  //% block="Y角度" group="体育"
+  export function axis_y():number{
+    return input.acceleration(Dimension.Y)/1100*90;
+  }
+
+ //% blockId=sport_habatobi_z
+  //% block="Z角度" group="体育"
+  export function axis_z():number{
+    return input.acceleration(Dimension.Z)/1100*90;
   }
 }
 

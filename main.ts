b22f2350c4447c60aff69520eeafaeb8c2f6d331
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-27 21:09:47 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -30,4 +30,15 @@ namespace matubara_blocks {
             return false;
         }
     }    
+
+ //% block="腕をふる角度が |%limit| 度より小きく" group="体育"
+ //% limit.min=0 limit.max=90
+  export function ude_small(limit: number): boolean {
+        if ((input.acceleration(Dimension.X)/1100*90) < limit ){
+            return true;
+            }else{
+            return false;
+        }
+    }   
+
 }

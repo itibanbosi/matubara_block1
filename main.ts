4c0fa88f1b396dd7fbba4ac074cdac6c1a172b58
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-28 19:04:22 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,26 +2,26 @@
 namespace matubara_blocks {
 
 
-  //% weight=10 blockId=sport_habatobi_x
+  //% weight=30 blockId=sport_habatobi_x
   //% block="X角度" group="体育"
   export function axis_x():number{
     return input.acceleration(Dimension.X)/1100*90;
   }
 
- //% weight=9 blockId=sport_habatobi_y
+ //% weight=29 blockId=sport_habatobi_y
   //% block="Y角度" group="体育"
   export function axis_y():number{
     return input.acceleration(Dimension.Y)/1100*90;
   }
 
- //% weight=8 blockId=sport_habatobi_z
+ //% weight=28 blockId=sport_habatobi_z
   //% block="Z角度" group="体育"
   export function axis_z():number{
     return input.acceleration(Dimension.Z)/1100*90;
   }
 
 
- //% weight=30 block="腕をふる角度が |%limit| 度より大きく" group="体育"
+ //% weight=19 block="腕をふる角度が |%limit| 度より大きく" group="体育"
  //% limit.min=0 limit.max=90
   export function ude_more(limit: number): boolean {
         if ((input.acceleration(Dimension.X)/1100*90) > limit ){
@@ -31,7 +31,7 @@ namespace matubara_blocks {
         }
     }    
 
- //% weight=6 block="腕をふる角度が |%limit| 度より小きく" group="体育"
+ //% weight=18 block="腕をふる角度が |%limit| 度より小きく" group="体育"
  //% limit.min=0 limit.max=90
   export function ude_small(limit: number): boolean {
         if ((input.acceleration(Dimension.X)/1100*90) < limit ){

06188eacc5a9e8b26c852d4dce56affc7a4acb9f
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-28 19:00:11 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ namespace matubara_blocks {
   }
 
 
- //% weight= 7 block="腕をふる角度が |%limit| 度より大きく" group="体育"
+ //% weight= 30 block="腕をふる角度が |%limit| 度より大きく" group="体育"
  //% limit.min=0 limit.max=90
   export function ude_more(limit: number): boolean {
         if ((input.acceleration(Dimension.X)/1100*90) > limit ){

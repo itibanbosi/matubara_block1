66ee2ebbf9412deac203c0e63c0f908233588a93
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-29 17:15:34 +0900

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,14 @@
 
 
 
-//% color="#3943c6" block="ﾕｰﾚｶ車" icon="\uf1b9"
-namespace matubara_blocks_car {
+//% color="#3943c6" block="幅跳び" icon="\uf1b9"
+namespace matubara_blocks {
 
 
   //% blockId=sport_habatobi
   //% block="幅跳び" group="スポーツテスト"
-  export function habatobi(): void {
-    return acceleration(Dimension.Y)/1100*90
+  export function habatobi():number{
+    return input.acceleration(Dimension.Y);
+
   }
-
-
 }

3537a59fa67a1844d1d24e498608bdad558d6047
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-29 17:06:03 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,9 +21,9 @@ namespace matubara_blocks {
   }
 
 
- //% weight=19 block="腕をふる角度が |%limit| 度より大きく" group="体育"
+ //% weight=19 block="X軸＿腕をふる角度が |%limit| 度より大きく" group="体育"
  //% limit.min=0 limit.max=90
-  export function ude_more(limit: number): boolean {
+  export function x_ude_more(limit: number): boolean {
         if ((input.acceleration(Dimension.X)/1100*90) > limit ){
             return true;
             }else{
@@ -31,14 +31,51 @@ namespace matubara_blocks {
         }
     }    
 
- //% weight=18 block="腕をふる角度が |%limit| 度より小きく" group="体育"
+ //% weight=18 block="X軸＿腕をふる角度が |%limit| 度より小きく" group="体育"
  //% limit.min=0 limit.max=90
-  export function ude_small(limit: number): boolean {
+  export function x_ude_small(limit: number): boolean {
         if ((input.acceleration(Dimension.X)/1100*90) < limit ){
             return true;
             }else{
             return false;
         }
     }   
+ //% weight=19 block="ｙ軸＿腕をふる角度が |%limit| 度より大きく" group="体育"
+ //% limit.min=0 limit.max=90
+  export function y_ude_more(limit: number): boolean {
+        if ((input.acceleration(Dimension.Y)/1100*90) > limit ){
+            return true;
+            }else{
+            return false;
+        }
+    }    
 
+ //% weight=18 block="ｙ軸＿腕をふる角度が |%limit| 度より小きく" group="体育"
+ //% limit.min=0 limit.max=90
+  export function y_ude_small(limit: number): boolean {
+        if ((input.acceleration(Dimension.Y)/1100*90) < limit ){
+            return true;
+            }else{
+            return false;
+        }
+    }   
+ //% weight=19 block="ｚ軸＿腕をふる角度が |%limit| 度より大きく" group="体育"
+ //% limit.min=0 limit.max=90
+  export function z_ude_more(limit: number): boolean {
+        if ((input.acceleration(Dimension.Z)/1100*90) > limit ){
+            return true;
+            }else{
+            return false;
+        }
+    }    
+
+ //% weight=18 block="ｚ軸＿腕をふる角度が |%limit| 度より小きく" group="体育"
+ //% limit.min=0 limit.max=90
+  export function z_ude_small(limit: number): boolean {
+        if ((input.acceleration(Dimension.Z)/1100*90) < limit ){
+            return true;
+            }else{
+            return false;
+        }
+    }   
 }
